bb77aeeac4e07b27b7af854f1154b18aa692e2a7
ariuna <eringarm97@gmail.com>
2022-07-01 00:28:49 -0400

    completed end2end project

--- src/main/java/com/academy/techcenture/end2end/CommonUtils.java
+++ src/main/java/com/academy/techcenture/end2end/CommonUtils.java
@@ -1,9 +1,57 @@
 package com.academy.techcenture.end2end;
-
 import com.github.javafaker.Faker;
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 public class CommonUtils {
 
+    public static String randomZip(){
+        return new Faker().address().zipCode().substring(0,5);
+    }
+
+
+    public static String randomPhoneNumber(){
+        return String.format(("(%03d) %3d-%4d"),
+                (int)Math.floor(999*Math.random()),
+                (int)Math.floor(999*Math.random()),
+                (int)Math.floor(999*Math.random()));
+    }
+
+    public static String generateRandomString(int limit){
+        String SALTCHARS = "ABCDEFJHIJKLMNOPQRSTUVWXYZ1234567890".toLowerCase(Locale.ROOT);
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < limit) {
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+    }
+
+    public static String randomAddress(){
+        return new Faker().address().streetAddress();
+    }
+
+    public static String randomCompanyName(){
+        return new Faker().company().name();
+    }
+
+    public static String randomState(){
+        return new Faker().address().state();
+    }
+
+    public static String randomCity(){
+        return new Faker().address().city();
+    }
+
+    public static int randomNumber(int to, int from){
+
+        return (int)(Math.random() * (to - from + 1) + from);
+    }
     public static String randomEmail(){
 
         Faker faker = new Faker();
@@ -14,5 +62,25 @@ public class CommonUtils {
         String email = lastName+"."+firstName+"@"+ domain[(int)(Math.random()*(4))]+".com";
 
         return email.toLowerCase();
+    }
+
+    public static String randomDOB18orAbove(){
+
+        LocalDate startDate = LocalDate.of(1958,1,1);
+        long start = startDate.toEpochDay();
+
+
+        LocalDate endDate = LocalDate.of(LocalDate.now().getYear() - 18, 1,1);
+        long end = endDate.toEpochDay();
+
+        long randomEpochDay = ThreadLocalRandom.current().longs(start, end).findAny().getAsLong();
+        return LocalDate.ofEpochDay(randomEpochDay).toString();
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(randomDOB18orAbove());
+
     }
 }
--- src/main/java/com/academy/techcenture/end2end/UserRegistrationEnd2End.java
+++ src/main/java/com/academy/techcenture/end2end/UserRegistrationEnd2End.java
@@ -1,14 +1,15 @@
 package com.academy.techcenture.end2end;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import jdk.swing.interop.SwingInterOpUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
-import java.util.Locale;
+
+import static com.academy.techcenture.end2end.CommonUtils.*;
 
 public class UserRegistrationEnd2End {
 
@@ -28,8 +29,7 @@ public class UserRegistrationEnd2End {
 
         String title = driver.getTitle();
 
-        if (title.equals("My Store"))
-        {
+        if (title.equals("My Store")) {
             System.out.println("Title is correct");
         } else {
             System.out.println("Title is incorrect");
@@ -37,11 +37,10 @@ public class UserRegistrationEnd2End {
 
         WebElement signInLink = driver.findElement(By.linkText("Sign in"));
 
-        if (signInLink.isDisplayed()){
+        if (signInLink.isDisplayed()) {
             System.out.println("Clicking sign in link, because it exists");
             signInLink.click();
-        }else
-        {
+        } else {
             System.out.println("Sign in link does not exist on the page");
         }
 
@@ -58,8 +57,7 @@ public class UserRegistrationEnd2End {
         Thread.sleep(1000);
         title = driver.getTitle();
 
-        if (title.equals("Login - My Store"))
-        {
+        if (title.equals("Login - My Store")) {
             System.out.println("Second page title is correct");
         } else {
             System.out.println("Second page title is NOT correct");
@@ -67,23 +65,22 @@ public class UserRegistrationEnd2End {
 
         WebElement mainHeader = driver.findElement(By.className("page-heading"));
 
-        if (mainHeader.getText().equals("CREATE AN ACCOUNT")){
+        if (mainHeader.getText().equals("CREATE AN ACCOUNT")) {
 
             System.out.println("Header is correct");
-        }
-        else System.out.println("nope");
+        } else System.out.println("nope");
 
-        int randomGender = (int) (Math.random()* (3 - 1) + 1);
+        int randomGender = (int) (Math.random() * (3 - 1) + 1);
 
-        WebElement genderRadioBtn = driver.findElement(By.xpath("//input[@id='id_gender"+randomGender+"']"));
+        WebElement genderRadioBtn = driver.findElement(By.xpath("//input[@id='id_gender" + randomGender + "']"));
 
         genderRadioBtn.click();
 
         int atSign = emailAddress.indexOf("@");
         String[] fullName = emailAddress.substring(0, atSign).split("\\.");
-        String firstName = fullName[1].substring(0,1).toUpperCase() + fullName[1].substring(1);
-        String lastName = fullName[0].substring(0,1).toUpperCase() + fullName[0].substring(1);
-        String password = lastName+ "123" + firstName.charAt(0) + "!!";
+        String firstName = fullName[1].substring(0, 1).toUpperCase() + fullName[1].substring(1);
+        String lastName = fullName[0].substring(0, 1).toUpperCase() + fullName[0].substring(1);
+        String password = lastName + "123" + firstName.charAt(0) + "!!";
 
 
         WebElement firstNameInput = driver.findElement(By.id("customer_firstname"));
@@ -97,17 +94,84 @@ public class UserRegistrationEnd2End {
 
         //compare the randomly generated email with populated email on the registration page
 
-        if (emailValueRegistration.equals(emailAddress)){
+        if (emailValueRegistration.equals(emailAddress)) {
             System.out.println("Email ids are correct");
-        }
-        else {
+        } else {
             System.out.println("Email ids do not match");
         }
 
         WebElement passwdInput = driver.findElement(By.id("passwd"));
         passwdInput.sendKeys(password);
 
+        String dob = randomDOB18orAbove();
 
+        String[] split = dob.split("-");
+        int year = Integer.parseInt(split[0]);
+        int month = Integer.parseInt(split[1]);
+        int day = Integer.parseInt(split[2]);
+
+
+        WebElement days = driver.findElement(By.id("days"));
+        WebElement months = driver.findElement(By.id("months"));
+        WebElement years = driver.findElement(By.id("years"));
+
+        Select select = new Select(days);
+        select.selectByValue(String.valueOf(day));
+
+        select = new Select(months);
+        select.selectByValue(String.valueOf(month));
+
+        select = new Select(years);
+        select.selectByValue(String.valueOf(year));
+
+        WebElement newsletter = driver.findElement(By.id("newsletter"));
+        WebElement specialOffers = driver.findElement(By.id("optin"));
+
+        if (!newsletter.isSelected()) {
+            System.out.println("Checked newsletter checkbox");
+            newsletter.click();
+        } else {
+            System.out.println("Newsletter is already checked");
+
+        }
+        if (!specialOffers.isSelected()) {
+            System.out.println("Checked special offers checkbox");
+            newsletter.click();
+        } else {
+            System.out.println("Special offers is already checked");
+        }
+
+        String companyName = randomCompanyName();
+        WebElement companyN = driver.findElement(By.xpath("//input[@name='company']"));
+        companyN.sendKeys(companyName);
+
+        WebElement postcode = driver.findElement(By.id("postcode"));
+        postcode.sendKeys(randomZip());
+
+        WebElement address1 = driver.findElement(By.id("address1"));
+        address1.sendKeys(randomAddress());
+
+        WebElement city = driver.findElement(By.id("city"));
+        city.sendKeys(randomCity());
+
+        WebElement stateDropDown = driver.findElement(By.id("id_state"));
+        select = new Select(stateDropDown);
+        String randomState = randomState();
+        select.selectByVisibleText(randomState);
+
+        WebElement addInfo = driver.findElement(By.id("other"));
+        addInfo.sendKeys(generateRandomString(20));
+
+        WebElement phone = driver.findElement(By.id("phone_mobile"));
+        phone.sendKeys(randomPhoneNumber());
+
+        WebElement alias = driver.findElement(By.id("alias"));
+        alias.sendKeys(randomAddress());
+
+        Thread.sleep(3000);
+
+        WebElement registerBtn = driver.findElement(By.id("submitAccount"));
+        registerBtn.click();
 
     }
 }
